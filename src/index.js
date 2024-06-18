59a06bb5834c02ed387adea13db45b977b9ad16f
Lizzie Siegle <esiegle@brynmawr.edu>
2024-06-18 11:22:47 -0700

    music playing no ai

--- src/index.js
+++ src/index.js
@@ -1,15 +1,64 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import html from '../static/webpage.html';
+import MidiWriter from 'midi-writer-js';
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+    async fetch(request, env) {
+        const url = new URL(request.url);
+
+        if (request.method === 'GET' && url.pathname === '/') {
+            return new Response(html, {
+                headers: { 'Content-Type': 'text/html' },
+            });
+        } else if (request.method === 'POST' && url.pathname === '/generate') {
+            const { drumPattern, bassline, synthesizers, effects, subgenre } = await request.json();
+
+            // Generate MIDI file based on user input
+            const track = new MidiWriter.Track();
+            track.addEvent(new MidiWriter.ProgramChangeEvent({ instrument: 1 }));
+
+            // Add drum pattern
+            track.addEvent(new MidiWriter.NoteEvent({ 
+                pitch: ['C4'],
+                duration: '4',
+                velocity: drumPattern / 100,
+                repeat: 4
+            }));
+
+            // Add bassline
+            track.addEvent(new MidiWriter.NoteEvent({ 
+                pitch: ['E2'],
+                duration: '1',
+                velocity: bassline / 100,
+                repeat: 2
+            }));
+
+            // Add synthesizers
+            track.addEvent(new MidiWriter.NoteEvent({ 
+                pitch: ['G4'],
+                duration: '2',
+                velocity: synthesizers / 100,
+                repeat: 2
+            }));
+
+            // Add effects
+            track.addEvent(new MidiWriter.NoteEvent({ 
+                pitch: ['C5'],
+                duration: '8',
+                velocity: effects / 100,
+                repeat: 8
+            }));
+
+            const write = new MidiWriter.Writer(track);
+            const midiData = write.buildFile();
+
+            return new Response(midiData, {
+                headers: { 
+                    'Content-Type': 'audio/midi',
+                    'Content-Disposition': 'attachment; filename="generated_track.mid"' 
+                },
+            });
+        }
+
+        return new Response('Not found', { status: 404 });
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'worker.js',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  target: 'webworker',
+  mode: 'development'
+};
